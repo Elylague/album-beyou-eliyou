1ab1c9ebc82fc554f85d42e5de32d3646293419b
Elylague <elyseelaguerre@gmail.com>
2024-11-25 17:41:42 -0500

    ajouter webpack et le fichier .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports= {
+  mode:'development',
+  entry:'./src/main.js',
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename:'bundle.js'
+    
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+      ],
+  },
+  
+  
+}
